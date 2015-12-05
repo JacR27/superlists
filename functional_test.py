f8763622cdee7adbd226bd75d018e03a77578625
Joe Rayner <jacryaner58@gmail.com>
2015-12-05 16:27:24 +0000

    functional test using unittest and userstory

--- functional_test.py
+++ functional_test.py
@@ -1,5 +1,40 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_post_and_get_to_do_items(self):
+        
+        # go to home page
+        self.browser.get('http://localhost:8000')
+
+        # title and header had to-do
+        self.assertIn('To-Do', self.browser.title)
+        self.fail("finish test")
+        #enter item box
+
+        #item entered
+
+        #display item
+
+        # ability to enter another item
+
+        # enter second item
+
+        # both items show on page
+
+        # unique url displayed
+
+        # url shows list
+
+        # done
+
+if __name__=="__main__":
+    unittest.main(warnings='ignore')
