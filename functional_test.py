70f860fbfa30d1519a3c9747203398d8a7b9b40d
Joe Rayner <jacryaner58@gmail.com>
2015-12-06 02:36:53 +0000

    Functional test update: checks we can enter and display items

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 import unittest
-
+from selenium.webdriver.common.keys import Keys
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
@@ -17,13 +17,24 @@ class NewVisitorTest(unittest.TestCase):
 
         # title and header had to-do
         self.assertIn('To-Do', self.browser.title)
-        self.fail("finish test")
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
         #enter item box
-
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+            )
         #item entered
-
+        inputbox.send_keys('hello')
+        inputbox.send_keys(Keys.ENTER)
         #display item
-
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.test == '1: hello')
+        )
+        
         # ability to enter another item
 
         # enter second item
@@ -35,6 +46,6 @@ class NewVisitorTest(unittest.TestCase):
         # url shows list
 
         # done
-
+        self.fail("finish test")
 if __name__=="__main__":
     unittest.main(warnings='ignore')
