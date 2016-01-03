942191611457d2e003b8ebab88d595bb9e757c46
Joe Rayner <jacryaner58@gmail.com>
2016-01-03 11:17:22 +0000

    subcommit end of holiday

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-    print("hello")
+    item = Item()
+    item.text =  request.POST.get('item_text', '')
+    item.save()
     return render(request, 'home.html', {'new_item_text': request.POST.get('item_text', '')})
